cda772f7081c49dca986354a66a5dc58305bb56e
Jolan <jolan.brizard@epitech.eu>
2024-01-30 10:16:37 +0100

    setup from starter

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   ignorePatterns: ["apps/**", "packages/**"],
-  extends: ["@fastbun-starter/eslint-config/library.js"],
+  extends: ["@jolan-dev/eslint-config/library.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
