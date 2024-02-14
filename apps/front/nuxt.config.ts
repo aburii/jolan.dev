ad8acb6fa0201849db423aa271abb77714f54972
Jolan <jolan.brizard@epitech.eu>
2024-02-14 10:40:49 +0100

    feature(package): install nuxtui

--- apps/front/nuxt.config.ts
+++ apps/front/nuxt.config.ts
@@ -1,7 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  
+
+  modules: ['@nuxt/ui'],
+
   typescript: {
     strict: true,
     shim: false,
