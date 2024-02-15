16864ea8aed3f8a6a03b101bb6e9490a70813b1f
Jolan <jolan.brizard@epitech.eu>
2024-02-15 14:19:29 +0100

    Feature/wip page (#1)
    * feature(package): install nuxtui
    
    * feature(page): wip page using nuxtui progress

--- apps/front/app.vue
+++ apps/front/app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <UContainer class="h-full">
+    <div class="max-w-5xl mx-auto h-full flex flex-col justify-center">
+      <h1 class="text-3xl mb-3 text-center">coming soon...</h1>
+      <UProgress animation="carousel" />
+    </div>
+  </UContainer>
 </template>
--- apps/front/nuxt.config.ts
+++ apps/front/nuxt.config.ts
@@ -1,7 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  
+
+  app: {
+    head: {
+      title: 'Jolan - Web Developer',
+    },
+  },
+
+  modules: ['@nuxt/ui'],
+
+  css: ['~/assets/scss/main.scss'],
+
   typescript: {
     strict: true,
     shim: false,
