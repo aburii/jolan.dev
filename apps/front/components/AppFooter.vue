d3e616c03ad2de0efeed57025dc2e3ddc0d2bb05
Jolan <jolan.brizard@gmail.com>
2024-06-18 17:46:41 +0200

    feat: app footer

---
+++ apps/front/components/AppFooter.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const currentYear = new Date().getFullYear();
+</script>
+
+<template>
+  <div
+    class="h-[50px] flex text-cod-gray-500 px-10 items-center border-t border-storm-dust-950"
+  >
+    <span class="text-start text-sm"
+      >© Jolan Brizard, {{ currentYear }} - All rights reserved</span
+    >
+  </div>
+</template>
+
+<style scoped></style>
