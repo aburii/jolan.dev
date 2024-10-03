165dc1e6de1fa670e6e4b4724178071ba270d22c
Jolan <jolan.brizard@gmail.com>
2024-10-03 17:10:59 +0200

    feat: project detail page and some refinements

--- apps/front/components/Home/HomeAbout.vue
+++ apps/front/components/Home/HomeAbout.vue
@@ -3,25 +3,28 @@
 </script>
 
 <template>
-  <div class="h-screen">
+  <div id="about" class="h-screen">
     <div class="flex h-full flex-col py-10 md:flex-row md:items-center">
       <NuxtImg class="lg:1/4 h-1/4 w-full rounded-xl object-cover md:h-full md:w-1/3" src="/images/FullSizeRender.png" />
       <div class="mt-10 w-full space-y-5 md:px-20">
-        <h2 class="font-display text-4xl font-black">
+        <h2 class="font-brand text-4xl font-black">
           About me
         </h2>
-        <div class="flex w-full flex-col items-center justify-center md:flex-row">
-          <p class="self-start font-extralight">
+        <div class="flex w-full flex-col items-center md:flex-row">
+          <p class="self-start font-display font-extralight">
             I'm a 23 year old developer based in Paris.
           </p>
-          <div class="ml-0 mt-10 md:ml-10 md:mt-0">
-            <UButton
-              label="Visit Universe"
-              variant="solid"
-              color="black"
-              size="xl"
-              class="mx-auto block text-nowrap font-semibold md:mx-0"
-            />
+          <div class="ml-0 mt-28 md:ml-10 md:mt-0">
+            <NuxtLink
+              class="inline-flex h-[48px] items-center justify-center rounded-lg bg-cod-gray-100 px-4 font-semibold text-black md:h-[60px]"
+              to="/universe"
+            >
+              Into the verse
+              <UIcon
+                name="i-heroicons-arrow-right"
+                class="text ml-2 align-baseline"
+              />
+            </NuxtLink>
           </div>
         </div>
       </div>
