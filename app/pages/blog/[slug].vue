9503bc1ab0fe5e448366d3b1ae0507ebf5622222
Jolan <jolan.brizard@gmail.com>
2025-08-16 20:24:59 +0200

    enable blog content render

--- app/pages/blog/[slug].vue
+++ app/pages/blog/[slug].vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
 const route = useRoute()
+
+const { data: page } = await useAsyncData(route.path, () => {
+  return queryCollection('blog').path(route.path).first()
+})
 </script>
 
 <template>
-  {{ route.params.slug }}
+  <div class="px-20 py-10">
+    <template v-if="page">
+      <ContentRenderer :value="page" />
+    </template>
+    <template v-else>
+      <div class="empty-page">
+        <h1>Page Not Found</h1>
+        <p>Oops! The content you're looking for doesn't exist.</p>
+      </div>
+    </template>
+  </div>
 </template>
-
-<style scoped>
-
-</style>
