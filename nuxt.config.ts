49950c018381b2a2c08cbee9e441d764921a88b7
Jolan <jolan.brizard@gmail.com>
2025-08-17 21:08:57 +0200

    disable ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  ssr: true,
+  ssr: false,
 
   modules: [
     '@nuxt/ui',
